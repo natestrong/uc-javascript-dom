aa8b7fe26748fb5f8ed7b7d8cf80534ed7e4068c
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-20 21:52:53 -0700

    Create label element func

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ const app = document.getElementById('app');
 app.innerHTML = `
     <h1>JavaScript DOM</h1>
 `;
+
+const cats = ['Oliver', 'Maxwell', 'Sophie'];
+
+// cats.forEach(name => {
+//     const li = document.createElement('li');
+//     li.innerText = name;
+//     app.append(li);
+// });
+
+const fragment = document.createDocumentFragment();
+
+cats.forEach(name => {
+    const li = document.createElement('li');
+    li.innerText = name;
+    fragment.append(li);
+});
+app.append(fragment);
