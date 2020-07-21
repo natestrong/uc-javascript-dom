bdcfb42979d6dce3e1d4ff1590fd2a068e920903
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-20 20:04:51 -0700

    Working on section 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import '../assets/css/style.css';
+
+const app = document.getElementById('app');
+app.innerHTML = `
+    <h1>JavaScript DOM</h1>
+`;
+
+console.log(document);
+console.dir(document);
+
+console.log(document.documentElement);
+console.dir(document.documentElement);
+
+console.log('------');
+console.dir(document.head);
+console.dir(document.body);
+
+console.log('------');
