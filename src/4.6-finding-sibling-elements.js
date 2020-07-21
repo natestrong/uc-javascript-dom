cf5546d858a7002e625b5171565aceaf6c650726
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-21 09:12:03 -0700

    Finished section 4

---
+++ src/4.6-finding-sibling-elements.js
@@ -0,0 +1,22 @@
+import '../assets/css/style.css';
+
+const app = document.getElementById('app');
+app.innerHTML = `
+    <h1>JavaScript DOM</h1>
+    <ul id="list">
+        <li>Oliver</li>
+        <li>Maxwell</li>
+        <li>Sophie</li>
+        <li>Crosby</li>
+    </ul>
+`;
+
+const listItem = document.querySelector('#list li');
+
+// Any DOM nodes
+console.log(listItem.nextSibling);
+
+// Any Element Nodes
+console.log(listItem.nextElementSibling);
+
+console.log(listItem.previousElementSibling);
