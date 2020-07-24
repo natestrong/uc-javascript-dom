3f512599018d638492d60217b0b8acd0698010c6
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-24 09:01:38 -0700

    Setup final project html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import '../assets/css/style.css';
 
 const app = document.getElementById('app');
+
 app.innerHTML = `
-    <h1>JavaScript DOM</h1>
+    <div class="todos">
+        <div class="todos-header">
+            <h3 class="todos-title">Todo List</h3>
+            <div>
+                <p>You have <span class="todos-count"></span> items</p>
+                <button type="button" class="todos-clear" style="display: none;">
+                    Clear Completed
+                </button>
+            </div>
+        </div>
+        <form class="todos-form" name="todos">
+            <input type="text" placeholder="What's next?" name="todo">
+        </form>
+        <ul class="todos-list"></ul>
+    </div>
 `;
